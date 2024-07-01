3b586a38858460948154f808e1254cadd9f93b74
ARitaMRamos <1232493@isep.ipp.pt>
2024-07-01 22:36:28 +0100

    members tests update

--- src/automated-tests/members-tests.spec.ts
+++ src/automated-tests/members-tests.spec.ts
@@ -4,41 +4,67 @@ import request from "supertest";
 
 
 
-let accessToken: string; // variable to store the access token
+//let accessToken: string; // variable to store the access token
 let uri: string = "http://localhost:5001"; // defines uri to requests
 
-describe("Testing requests from members", () => {
+describe("Testing requests to members", () => {
 
     beforeAll(async (): Promise<void> => {
-        //Create an user
+        // To Create an user
         try {
             let createUserResponse = await request(uri)
-                .post("api/vi/users")
+                .post("api/v1/users")
                 .send({
                     username: "zzz",
-                    email: "zzz@zzz-com",
+                    email: "zzz@zzz.com",
                     password: "zzzzzz",
                 });
             let createUser = createUserResponse.body;
             console.log("Created User:", createUser);
         } catch (error) {
-            console.log("Create User Error", error);
+            console.log("Create User Error:", error);
         }
+
+        // User Loggin
+        let loginResponse = await request(uri)
+            .post("/api/v1/users/login")
+            .send({
+                username: "zzz",
+                password: "zzzzzz",
+      });
     })
 
-    test("Members if member information is ok", async () => {
+
+
+    test("If member is registred", async () => {
         // Arrange
         let username = "zzz";
         let expectedStatus = 200;
 
         //Send request - Act
         let response = await request(uri)
-            .get("/api/v1/members/${username}")
+            .get("/api/v1/members/:{username}")
             .set("Content-Type", "application/json")
-            .send();
+            .send({});
 
         //Request Response - Assert
         expect(response.status).toBe(expectedStatus)
-    })
+    });
+
+
+    test("If member is not registred", async () => {
+        // Arrange
+        let username = "yyy";
+        let expectedStatus = 404;
+
+        //Send request - Act
+        let response = await request(uri)
+            .get("/api/v1/members/:{username}")
+            .set("Content-Type", "application/json")
+            .send({});
+
+        //Request Response - Assert
+        expect(response.status).toBe(expectedStatus)
+    });
 
 })
